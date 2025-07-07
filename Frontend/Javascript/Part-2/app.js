a012abc38fd4714962071ddde61eec216e79c39e
rajankumar009 <rk1620703@gmail.com>
2025-07-07 22:04:32 +0530

    Complete Js Part-2

---
+++ Frontend/Javascript/Part-2/app.js
@@ -0,0 +1,14 @@
+let size = "XL";
+if ( size === "XL" ){
+    console.log("Popcorn Price is 250rs");
+    
+}else if ( size === "L" ){
+    console.log("Popcorn Price is 200rs");
+    
+}else if ( size === "M" ){
+    console.log("Popcorn Price is 100rs");
+    
+}else if ( size === "S" ){
+    console.log("Popcorn Price is 50rs");
+    
+}
