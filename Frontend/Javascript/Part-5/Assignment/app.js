47697577a8fcde16a094d1d45c7c6d384b553719
rajankumar009 <rk1620703@gmail.com>
2025-07-13 10:07:02 +0530

    Assignment

---
+++ Frontend/Javascript/Part-5/Assignment/app.js
@@ -0,0 +1,20 @@
+// Qs - 1
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+console.log(diceRoll);
+
+// Qs - 2
+let car = {
+    name:"maruty",
+    model:2018,
+    color:"red"
+}
+console.log(car.name);
+
+// Qs - 3
+let inFo = {
+    name:"rajan",
+    age:23,
+    city:"sre"
+}
+inFo.city = 'New York';
+console.log(inFo);
