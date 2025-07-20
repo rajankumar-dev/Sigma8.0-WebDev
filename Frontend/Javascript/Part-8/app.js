27a3537c11b2cd03a1e747b752a5702bc06da91a
rajankumar009 <rk1620703@gmail.com>
2025-07-20 22:59:38 +0530

    complete part 8 in js

---
+++ Frontend/Javascript/Part-8/app.js
@@ -0,0 +1,14 @@
+let arr = [1,2,3,4];
+arr.forEach(item => {
+    // console.log(item);
+});
+
+let print = function(el){
+    // console.log(el);
+}
+arr.forEach(print);
+
+// Map function in javascript
+//Return a new array and does not affect the orgnl array
+ let num = [1,2,3,4,5];
+
