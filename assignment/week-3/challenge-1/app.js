c6522007eb4a4784aaa0a79c81898c581141857a
rajankumar009 <rk1620703@gmail.com>
2025-07-27 09:57:38 +0530

    Level up Functionality Complete in Simon Says Game

---
+++ assignment/week-3/challenge-1/app.js
@@ -0,0 +1,31 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow" , "red", "Purple", "Green"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", () => {
+    if(started == false){
+        console.log("game started");
+        started = true;
+        levelUp();
+    }
+});
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(() => {
+        btn.classList.remove("flash");
+    },1000);
+}
+
+function levelUp(){
+    level++;
+    h2.innerText = `level ${level}`;
+
+    btnFlash();
+}
