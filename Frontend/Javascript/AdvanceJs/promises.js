eb622617c8a2eac41307aa6160c85c4abb9e5f2e
rajankumar009 <rk1620703@gmail.com>
2025-07-28 23:06:48 +0530

    Complete promises in js

---
+++ Frontend/Javascript/AdvanceJs/promises.js
@@ -0,0 +1,19 @@
+let onePromise = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+        //console.log('Async task is complete');
+        resolve();
+    },1000);
+});
+onePromise.then(() => {
+    //console.log('promises completed');
+});
+
+// Second Promise
+let promiseTwo = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({userName:'rajan', email:'rajan@gmail.com'});
+    },1000);
+});
+promiseTwo.then((user) => {
+    console.log(user);
+})
