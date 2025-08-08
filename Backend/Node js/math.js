a27a167630f10043d9fa69379e544695c4356184
rajankumar-dev <rk1620703@gmail.com>
2025-08-08 19:46:46 +0530

    Learn Import Statement

--- Backend/Node js/math.js
+++ Backend/Node js/math.js
@@ -1,7 +1,9 @@
-const sum = (a,b) => a+b;
-const sub = (a,b) => a-b;
-const mul = (a,b) => a*b;
-const div = (a,b) => a/b;
+export const sum = (a,b) => a+b;
+export const sub = (a,b) => a-b;
+export const mul = (a,b) => a*b;
+export const div = (a,b) => a/b;
 
 //Export File
 module.exports = 123;
+
+
--- Backend/Node js/nodeBasics.js
+++ Backend/Node js/nodeBasics.js
@@ -1,6 +1,9 @@
 // Process Object
 console.log(process.argv);
 
-//Import File
+//Require File
 const mathVal = require("./math");
 console.log(mathVal);
+
+//Import Statement
+import {sum, mul} from "./math.js"
