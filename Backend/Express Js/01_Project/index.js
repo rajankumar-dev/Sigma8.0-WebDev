b26c990034c642fed982da8ab9def4113e383fa4
rajankumar-dev <rk1620703@gmail.com>
2025-08-10 16:48:20 +0530

    learn about Express

--- Backend/Express Js/01_Project/index.js
+++ Backend/Express Js/01_Project/index.js
@@ -5,7 +5,26 @@ let port = 3000;
 app.listen(port, () =>{
     console.log("app is listen on localhost:",port);
 });
-app.use((req, res) => {
-    console.log("recieved");
-    res.send("response")
+
+//Express Routs
+app.get("/", (req, res)=>{
+    res.send("YOu Enter the root path")
+});
+app.get("/help", (req, res)=>{
+    res.send("YOu Enter the root help");
+});
+app.get("/apple", (req, res)=>{
+    res.send("YOu Enter the root apple")
+});
+
+//path parameters
+app.get("/:username/:id", (req, res)=>{
+    // console.log(req.params);
+    // res.send(`You enter the`);
+});
+
+//Query String
+app.get("/search", (req, res)=>{
+    let {q} = (req.query);
+    res.send(q);
 });
