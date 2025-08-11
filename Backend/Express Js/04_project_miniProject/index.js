8ebb3c3d587222e0386b34e34273f8d893cf6138
rajankumar-dev <rk1620703@gmail.com>
2025-08-11 21:41:58 +0530

    learn about data parse via backend to frontend

---
+++ Backend/Express Js/04_project_miniProject/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use("/", (req, res) => {
+    res.render("index");
+});
