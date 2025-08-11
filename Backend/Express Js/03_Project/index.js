cb890a84304565f0d8dec0cce1e43b48022d399d
rajankumar-dev <rk1620703@gmail.com>
2025-08-11 14:34:49 +0530

    learn about cookie-parser

---
+++ Backend/Express Js/03_Project/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use("/", (req, res) => {
+    res.send("This is Home page");
+});
