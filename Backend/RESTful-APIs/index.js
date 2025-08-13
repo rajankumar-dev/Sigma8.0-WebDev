ae314f6f27c1d0742f370958210a8b6e45ce11af
rajankumar-dev <rk1620703@gmail.com>
2025-08-13 22:53:39 +0530

    Complete Functionality on Showing all post

--- Backend/RESTful-APIs/index.js
+++ Backend/RESTful-APIs/index.js
@@ -4,11 +4,40 @@ const path = require("path");
 
 const port = 8080;
 
+app.use(express.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set(express.static(path.join(__dirname, "public")));
+let posts = [
+    {
+        username:"rajan kumar",
+        content:"Jai Mata Di"
+    },
+    {
+        username:"rahul kumar",
+        content:"Jai Mata Di"
+    },
+    {
+        username:"raju kumar",
+        content:"Jai Mata Di"
+    }
+];
 
 app.listen(port, () => {
     console.log("Listen on port :", port);
-})
+});
+
+app.get("/posts", (req, res) => {
+    res.render("index", {posts});
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new");
+});
+app.post("/posts", (req, res) => {
+    res.send("post req working");
+    console.log(req.body);
+    
+});
