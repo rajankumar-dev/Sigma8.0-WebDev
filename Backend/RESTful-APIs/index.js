c35e4af8fc0ce41f9591cd3209ec393ad3144885
rajankumar-dev <rk1620703@gmail.com>
2025-08-14 22:26:04 +0530

    complete post id functionality

--- Backend/RESTful-APIs/index.js
+++ Backend/RESTful-APIs/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const {v4: uuidv4} = require("uuid");
 
 const port = 8080;
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id:uuidv4(),
         username:"rajan kumar",
         content:"Jai Mata Di"
     },
     {
+        id:uuidv4(),
         username:"rahul kumar",
         content:"Jai Mata Di"
     },
     {
+        id:uuidv4(),
         username:"raju kumar",
         content:"Jai Mata Di"
     }
@@ -40,5 +44,10 @@ app.post("/posts", (req, res) => {
     let {username, content} = req.body;
     posts.push({username, content});
     res.redirect("/posts");
-    
 });
+app.get("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show", {post});
+});
+
