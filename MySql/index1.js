8e33e2e96c6f673c261f645299188a3271a001ad
rajankumar-dev <rk1620703@gmail.com>
2025-08-22 22:04:14 +0530

    learn more about sql

--- MySql/index1.js
+++ MySql/index1.js
@@ -1,6 +1,15 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app2',
+    password:"1128"
+});
 
 app.get("/", (req,res) => {
     res.send("welcome to homepage");
