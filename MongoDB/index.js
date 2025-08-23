c29c1f6b0c04e6b2ccbb76c2db54c41cc5396ff4
rajankumar-dev <rk1620703@gmail.com>
2025-08-23 22:34:27 +0530

    Create First Schema

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
+//Crete a connection to MongoDB
 main().then(() => console.log('Connected to MongoDB'))
 .catch(err => console.log(err));
-
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+//Create a schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String
+});
+
