46c3249232df07e871e90a84959fec9f13f67af4
rajankumar-dev <rk1620703@gmail.com>
2025-08-25 21:12:20 +0530

    Complete Book Database

--- MongoDB/books.js
+++ MongoDB/books.js
@@ -7,3 +7,44 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
 
+//Schema for book
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author:{
+        type: String,
+    },
+    price: {
+        type: Number,
+    }
+});
+
+//Create a Model for Book Schema
+const Book = mongoose.model("Book", bookSchema);
+
+let book1 = new Book({
+    title:"mathmatics",
+    author:"rd Sharma",
+    price: 340
+});
+let book2 = new Book({
+    title:"mathmatics",
+    author:"rd Sharma",
+    price: 340
+});
+
+book1.save().then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+    
+})
+book2.save().then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+    
+})
+
