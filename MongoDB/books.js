e8cad6f23afa0a05ee70cab286ccb4cdcca0a424
rajankumar-dev <rk1620703@gmail.com>
2025-08-25 21:40:08 +0530

    Learn About Schema Validation

--- MongoDB/books.js
+++ MongoDB/books.js
@@ -18,33 +18,39 @@ const bookSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
+        min: [1, "Price to Low for Amazone selling"]
+    },
+    discount: {
+        type: Number,
+        default:0
     }
 });
 
 //Create a Model for Book Schema
 const Book = mongoose.model("Book", bookSchema);
 
-let book1 = new Book({
-    title:"mathmatics",
-    author:"rd Sharma",
-    price: 340
-});
-let book2 = new Book({
-    title:"mathmatics",
-    author:"rd Sharma",
-    price: 340
-});
+//update
+Book.findByIdAndUpdate('68ac818781d9e8e97cf679f5',{price:-166}, {runValidators: true});
+// let book1 = new Book({
+//     title:"mathmatics",
+//     author:"rd Sharma",
+//     price: 340
+// });
+// let book2 = new Book({
+//     title:"mathmatics",
+//     author:"rd Sharma",
+//     price: 340
+// });
 
-book1.save().then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
-    
-})
-book2.save().then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
-    
-})
+// book1.save().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// book2.save().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
 
