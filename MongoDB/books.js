0282b3823d2beb2d76d536f38eddc5fa8bab9e9a
rajankumar-dev <rk1620703@gmail.com>
2025-08-25 20:37:50 +0530

    create new book database

---
+++ MongoDB/books.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+//Crete a connection to MongoDB
+main().then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
