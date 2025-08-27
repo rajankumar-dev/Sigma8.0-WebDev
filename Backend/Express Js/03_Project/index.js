150b407b2227ad388b3617788d82801312950099
rajankumar-dev <rk1620703@gmail.com>
2025-08-27 22:51:54 +0530

    add new port

--- Backend/Express Js/03_Project/index.js
+++ Backend/Express Js/03_Project/index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
-const port = 8080;
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
