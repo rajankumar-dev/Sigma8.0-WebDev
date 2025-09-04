f5f90486b9d962b83b5dc58ce448cdee3f2e45b4
rajankumar-dev <rk1620703@gmail.com>
2025-09-04 22:31:59 +0530

    Write our First middleware

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.use( () => {
+    console.log("I am middleware");
+});
+
+app.get("/" , (req, res) => {
+    res.send("I am Root");
+})
+app.get("/random" , (req, res) => {
+    res.send("this is random page");
+})
+
+
 app.listen(8080, () => {
     console.log("Server is listening on 8080");
 })
