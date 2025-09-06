ed6d0589b51d2858f256f8e5355d7ea2aa07d92b
rajankumar-dev <rk1620703@gmail.com>
2025-09-06 21:12:54 +0530

    learn about Next() in middlleware

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use( () => {
+//Simple Middleware
+app.use( (req,res,next) => {
     console.log("I am middleware");
+    next();
 });
 
+//Next Middleware
+app.use((req,res,next) =>{
+    console.log("Middleware first");
+    next();
+})
+app.use((req,res,next) =>{
+    console.log("Middleware second");
+    next();
+})
 app.get("/" , (req, res) => {
     res.send("I am Root");
 })
@@ -12,7 +23,6 @@ app.get("/random" , (req, res) => {
     res.send("this is random page");
 })
 
-
 app.listen(3000, () => {
-    console.log("Server is listening on 8080");
+    console.log("Server is listening on 3000");
 })
