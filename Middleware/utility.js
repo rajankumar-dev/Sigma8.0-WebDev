81f2bffbe1febf2833b830062dcc4de82c255638
rajankumar-dev <rk1620703@gmail.com>
2025-09-07 21:04:35 +0530

    middleware utility

--- Middleware/utility.js
+++ Middleware/utility.js
@@ -2,14 +2,30 @@ const express = require("express");
 const app = express();
 
 app.use((req,res,next) => {
-
-    console.log(req.event);
+    // console.log(req.event);
     next();
 });
 
 app.get("/", (req, res) => {
     console.log("I am root");
 })
+
+//API token as Query String
+app.use("/api",(req, res, next) => {
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }else{
+        res.send("access denied");
+    }
+})
+
+// API Request
+app.get("/api", (req, res) => {
+    res.send("Data");
+})
+
+
 
 app.listen(8080, () => {
     console.log("listening on 8080");
