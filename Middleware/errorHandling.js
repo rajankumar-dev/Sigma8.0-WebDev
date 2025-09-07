1d650423545c3288cc68cc9530d4aecf81624d93
rajankumar-dev <rk1620703@gmail.com>
2025-09-07 21:57:59 +0530

    Learn Error Hndling by seprate class

---
+++ Middleware/errorHandling.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const ExpressError = require("./ExpressError");
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log("I am root");
+})
+
+//API token as Query String
+const checkToken = (req, res, next) => {
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }
+    throw new ExpressError(401, "Access Denied");
+    
+}
+// API Request
+app.get("/api", checkToken, (req, res) => {
+    res.send("Data");
+})
+
+// Custom error Route
+app.get("/err",(req, res) => {
+    abcd =abcd;
+})
+app.use((err, req, res, next) => {
+    let { status = 500 , message = "some Error Occurred"} = err;
+    res.status(status).send(message);
+})
+
+
+app.listen(8080, () => {
+    console.log("listening on 8080");
+});
