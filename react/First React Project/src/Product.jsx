2fb35be315a915bba614fa0bd5e15078f8de2010
rajankumar-dev <rk1620703@gmail.com>
2025-10-04 23:14:20 +0530

    create our first component

---
+++ react/First React Project/src/Product.jsx
@@ -0,0 +1,11 @@
+function Product({title, price}) {
+    return(
+    <>
+        <div>
+            <h2>{title}</h2>
+            <p>{price}</p>
+        </div>
+    </>
+    )
+}
+export default Product;
