b381b01adb484a3255227af38eb842450cfd2668
rajankumar-dev <rk1620703@gmail.com>
2026-01-27 22:13:10 +0530

    update some issue

--- Frontend/React/React-State/src/helper.js
+++ Frontend/React/React-State/src/helper.js
@@ -1,13 +1,13 @@
-function genTicket(n){
-    let arr = new Array(n);
-    for (let i = 0; i<n; i++){
-        arr[i] = Math.floor(Math.random() * 10);
-    }
-    return arr;
+function genTicket(n) {
+  let arr = new Array(n);
+  for (let i = 0; i < n; i++) {
+    arr[i] = Math.floor(Math.random() * 20);
+  }
+  return arr;
 }
 
-function sum(arr){
-    return arr.reduce((sum, curr) => sum + curr, 0);
+function sum(arr) {
+  return arr.reduce((sum, curr) => sum + curr, 0);
 }
 
-export{ genTicket, sum };
+export { genTicket, sum };
