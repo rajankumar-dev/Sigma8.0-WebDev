f1eca7f3c6d473541ca67dcf3535a1b1af250cc6
rajankumar-dev <rk1620703@gmail.com>
2026-07-18 22:41:20 +0530

    learning about conditional statements

---
+++ C++/Assignment/02_assignment/index.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+ int main(){
+
+    //Question 01
+    /*
+    int num;
+    cout << "Enter a Number :" << endl;
+    cin >> num ;
+
+    if(num < 0){
+        cout<< "Number is Negative"<<endl;
+    }else if(num > 0){
+        cout<< "Number is Positive"<<endl;
+    }else{
+        cout<< "Number is Zero"<<endl;
+    }
+    */
+
+    //Question 02
+    /*
+    int year;
+    cout << "Enter a year" << endl;
+    cin >> year;
+
+    if(year%400 == 0 || (year%4 == 0 && year%100 != 0 )){
+        cout << "This is leap year" <<endl;
+    }else{
+        cout << "this is not a leap year";
+    }
+    */
+
+    //Question 03
+    /*
+    int a=63, b=36;
+    bool x=(a<b)?true:false;
+    int y=(a>b)?a:b;
+    cout<<x<<","<<y<<endl;
+    */
+
+    //Question 04
+    /*
+    int a=5;
+    if(++a*5<=25){
+        cout<<"Hello\n";
+    }else{cout<<"Bye\n";
+    }
+    */
+    return 0;
+ }
