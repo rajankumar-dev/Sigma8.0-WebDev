ea3fafcd38121528e10ab27589e981482eb07def
rajankumar-dev <rk1620703@gmail.com>
2026-07-18 10:35:16 +0530

    fix(index): format code for consistency and readability

--- Backend/Express Js/01_Project/index.js
+++ Backend/Express Js/01_Project/index.js
@@ -2,29 +2,29 @@ const express = require("express");
 const app = express();
 let port = 3000;
 
-app.listen(port, () =>{
-    console.log("app is listen on localhost:",port);
+app.listen(port, () => {
+  console.log("app is listen on localhost:", port);
 });
 
 //Express Routs
-app.get("/", (req, res)=>{
-    res.send("YOu Enter the root path")
+app.get("/", (req, res) => {
+  res.send("YOu Enter the root path");
 });
-app.get("/help", (req, res)=>{
-    res.send("YOu Enter the root help");
+app.get("/help", (req, res) => {
+  res.send("YOu Enter the root help");
 });
-app.get("/apple", (req, res)=>{
-    res.send("YOu Enter the root apple")
+app.get("/apple", (req, res) => {
+  res.send("YOu Enter the root apple");
 });
 
 //path parameters
-app.get("/:username/:id", (req, res)=>{
-    // console.log(req.params);
-    // res.send(`You enter the`);
+app.get("/:username/:id", (req, res) => {
+  // console.log(req.params);
+  // res.send(`You enter the`);
 });
 
 //Query String
-app.get("/search", (req, res)=>{
-    let {q} = (req.query);
-    res.send(q);
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  res.send(q);
 });
