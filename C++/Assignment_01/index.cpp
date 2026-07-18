4b115ce8df7cbf7a1996a6cf0264dd2dedbaf766
rajankumar-dev <rk1620703@gmail.com>
2026-07-18 15:03:09 +0530

    feat(index): Complete Cpp Assignment 1

---
+++ C++/Assignment_01/index.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Question 01 square root
+
+    /*int side;
+    cout<< "Enter the one side of Square";
+    cin>>side;
+    cout<<side*side;
+    */
+
+    //Question 02 Some of pen pencil and eraser
+
+    /*
+    float pencil, pen, eraser;
+
+    cout<<"Enter the value of pencil :"<<endl;
+    cin>>pencil;
+    cout<<"Enter the value of pen :"<<endl;
+    cin>>pen;
+    cout<<"Enter the value of Eraser :"<<endl;
+    cin>>eraser;
+
+    float totalCost = pencil + pen + eraser;
+    cout<<"Total Cost :" << totalCost << endl;
+    cout<<"Total Cost with GST:" << totalCost + (0.18 * totalCost) << endl;
+    */
+
+    //Question 03 Simple interest
+
+    /*
+    float P, R, T;
+
+    cout << "Enter the Principal amount :" << endl;
+    cin >> P;
+    cout << "Enter the Rate of Interest :" << endl;
+    cin >> R;
+    cout << "Enter the Time in Year:" << endl;
+    cin >> T;
+
+    float simInterest = (P * R * T)/100;
+    cout << "Simple Interest is :" << simInterest << endl;
+    */
+
+    //Question 04
+    /*
+    float radius;
+    float pi = 3.14159;
+    cout << "Enter the radius of a circle" << endl;
+    cin >> radius;
+    
+    float area = pi * radius * radius;
+    cout << "The area of circle is :" << area << endl;
+    */
+    
+    return 0;
+}
