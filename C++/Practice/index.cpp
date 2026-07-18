30f3e1d73b148de1d2120ccace2e5c9a3602891e
rajankumar-dev <rk1620703@gmail.com>
2026-07-18 15:48:13 +0530

    learn about operators

--- C++/Practice/index.cpp
+++ C++/Practice/index.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main(){
-    cout << (bool)3 + 2 << endl;
-    cout << 25.5 + 2 + 'A';
+    // cout << (bool)3 + 2 << endl;
+    // cout << 25.5 + 2 + 'A';
+
+    int x=10, y=5;
+    int exp1=(y*(x/y+x/y));
+    int exp2=(y*x/y+y*x/y);
+    cout<<exp1<<"";
+    cout<<exp2<<"\n";
 
     return 0;
 }
