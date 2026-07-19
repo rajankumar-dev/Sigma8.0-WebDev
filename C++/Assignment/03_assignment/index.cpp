f6293cd8f0f3a49948d60f89c5e9cd4898dc5569
rajankumar-dev <rk1620703@gmail.com>
2026-07-19 19:20:20 +0530

    Refactor: Comment out unused code for number input and summation

--- C++/Assignment/03_assignment/index.cpp
+++ C++/Assignment/03_assignment/index.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 int main(){
 
-    int num;
-    cout << "Enter a Number :";
-    cin >> num;
-    int sum = 0;
-    for(int i = 0; i<=num; i++){
-        sum +=i;
+    // int num;
+    // cout << "Enter a Number :";
+    // cin >> num;
+    // int sum = 0;
+    // for(int i = 0; i<=num; i++){
+    //     sum +=i;
+    // }
+    // cout<<sum;
+
+    int i = 0;
+    while(i<=5){
+        cout<<"*";
+        
+        i++;
     }
-    cout<<sum;
 }
