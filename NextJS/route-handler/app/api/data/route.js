0660d9f2687a5dc2ec9d5176ad17db72d0cded90
rajankumar-dev <rk1620703@gmail.com>
2026-07-26 22:14:00 +0530

    add some missing data

--- NextJS/route-handler/app/api/data/route.js
+++ NextJS/route-handler/app/api/data/route.js
@@ -2,12 +2,14 @@ export async function POST(request) {
   const { title, completed } = await request.json(); // JSON parse karega
 
   //console.log(body);
+  //console.log(body);
 
   return Response.json({
     success: true,
     message: "Todo Created successfully",
     todo: {
       title,
+      completed,
       completed,
     },
   });
