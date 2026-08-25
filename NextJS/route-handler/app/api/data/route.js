3d55f18be8b3929920c09cd26dc37e3e931bac9b
rajankumar-dev <rk1620703@gmail.com>
2026-08-25 09:22:50 +0530

    update name

--- NextJS/route-handler/app/api/data/route.js
+++ NextJS/route-handler/app/api/data/route.js
@@ -2,14 +2,12 @@ export async function POST(request) {
   const { title, completed } = await request.json(); // JSON parse karega
 
   //console.log(body);
-  //console.log(body);
 
   return Response.json({
     success: true,
     message: "Todo Created successfully",
     todo: {
       title,
-      completed,
       completed,
     },
   });
